44d2351b5714a3ade7c257b59d93b688fe881bcd
riarumoda <justinebudiwijaya@gmail.com>
2026-06-08 02:27:18 +0000

    env: cleanup environment script
    Signed-off-by: riarumoda <justinebudiwijaya@gmail.com>

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 echo "- Setting up build environment..."
 
-# GCC and Clang settings
-export CLANG_ROOT="$PWD/clang"
-export GCC64_ROOT="$PWD/gcc64"
-export GCC32_ROOT="$PWD/gcc32"
-export PATH="$CLANG_ROOT/bin:$GCC64_ROOT/bin:$GCC32_ROOT/bin:/usr/bin:$PATH"
-TC_URLS_MAIN=(
-    "clang|https://api.github.com/repos/bachnxuan/aosp_clang_mirror/releases/latest"
-)
-TC_URLS_LEGACY=(
-    "clang|https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b.git"
-    "gcc64|https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git"
-    "gcc32|https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git"
-)
-TC_URLS_ALT=(
-    "clang|https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379.git"
-    "gcc64|https://api.github.com/repos/mvaisakh/gcc-build/releases/latest"
-    "gcc32|https://api.github.com/repos/mvaisakh/gcc-build/releases/latest"
-)
-
 # Device Default Exports
+echo "-- Exporting device settings..."
 export KBUILD_BUILD_USER=riarumoda-compile
 export KBUILD_BUILD_HOST=riaru.com
 export KERNEL_NAME="-perf-neon"
@@ -29,61 +11,38 @@ export MAIN_DEFCONFIG="arch/arm64/configs/vendor/sdmsteppe-perf_defconfig"
 export ACTUAL_MAIN_DEFCONFIG="vendor/sdmsteppe-perf_defconfig"
 export COMMON_DEFCONFIG="vendor/debugfs.config"
 export FEATURE_DEFCONFIG=""
-export TC_ALT_MODE=1
 
-# Device Settings - v3.5
+# Device Settings - v3.7
 case "$DEVICE_IMPORT" in
     # LineageOS
-    sweet|davinci|tucana|violet|sweet-droidspaces)
+    sweet|davinci)
         export DEVICE_DEFCONFIG="vendor/${DEVICE_IMPORT}.config"
-        if [ "$DEVICE_IMPORT" = "sweet-droidspaces" ]; then
-            export DEVICE_DEFCONFIG="vendor/sweet.config"
-            export KERNEL_NAME="-perf-droidspaces-neon"
-        fi
         ;;
-    ginkgo|laurel_sprout|ginkgo-droidspaces)
+    ginkgo|laurel_sprout)
         export MAIN_DEFCONFIG="arch/arm64/configs/vendor/trinket-perf_defconfig"
         export ACTUAL_MAIN_DEFCONFIG="vendor/trinket-perf_defconfig"
         export COMMON_DEFCONFIG="vendor/debugfs.config vendor/xiaomi-trinket.config"
         export DEVICE_DEFCONFIG="vendor/${DEVICE_IMPORT}.config"
-        if [ "$DEVICE_IMPORT" = "ginkgo-droidspaces" ]; then
-            export DEVICE_DEFCONFIG="vendor/ginkgo.config"
-            export KERNEL_NAME="-perf-droidspaces-neon"
-        fi
         export KBUILD_BUILD_USER=hiyorun-compile
         ;;
-    umi|umi-droidspaces)
+    umi)
         export MAIN_DEFCONFIG="arch/arm64/configs/vendor/kona-perf_defconfig"
         export ACTUAL_MAIN_DEFCONFIG="vendor/kona-perf_defconfig"
         export DEVICE_DEFCONFIG="vendor/xiaomi/sm8250-common.config vendor/xiaomi/${DEVICE_IMPORT}.config"
-        if [ "$DEVICE_IMPORT" = "umi-droidspaces" ]; then
-            export DEVICE_DEFCONFIG="vendor/xiaomi/sm8250-common.config vendor/xiaomi/umi.config"
-            export KERNEL_NAME="-perf-droidspaces-neon"
-        fi
         export KERNEL_VERSION="4.19"
         export KBUILD_BUILD_USER=kamilek-compile
         ;;
-    mi89x7-playground|mi89x7-droidspaces)
+    # Mi-Thorium
+    mi89x7-playground)
         export MAIN_DEFCONFIG="arch/arm64/configs/vendor/msm8937-perf_defconfig"
         export ACTUAL_MAIN_DEFCONFIG="vendor/msm8937-perf_defconfig"
         export COMMON_DEFCONFIG="vendor/msm8937-legacy.config vendor/common.config"
         export DEVICE_DEFCONFIG="vendor/xiaomi/msm8937/common.config vendor/xiaomi/msm8937/mi8937.config"
         if [ "$DEVICE_IMPORT" = "mi89x7-playground" ]; then
             export FEATURE_DEFCONFIG="vendor/feature/android-12.config vendor/feature/erofs.config vendor/feature/exfat.config vendor/feature/kprobes.config vendor/feature/lmkd.config vendor/feature/ntfs.config vendor/feature/wireguard.config"
-            export KERNEL_NAME="-Mi8937v2-neon"
-        elif [ "$DEVICE_IMPORT" = "mi89x7-droidspaces" ]; then
-            export FEATURE_DEFCONFIG="vendor/feature/android-12.config vendor/feature/erofs.config vendor/feature/exfat.config vendor/feature/kprobes.config vendor/feature/lmkd.config vendor/feature/ntfs.config vendor/feature/wireguard.config"
-            export KERNEL_NAME="-Mi8937v2-droidspaces-neon"
+            export KERNEL_NAME="-mithorium-neon"
         fi
         export KERNEL_VERSION="4.19"
-        ;;
-    a52s)
-        export MAIN_DEFCONFIG="arch/arm64/configs/vendor/lineage-a52sxq_defconfig"
-        export ACTUAL_MAIN_DEFCONFIG="vendor/lineage-a52sxq_defconfig"
-        export COMMON_DEFCONFIG=""
-        export DEVICE_DEFCONFIG=""
-        export FEATURE_DEFCONFIG=""
-        export KERNEL_VERSION="5.4"
         ;;
     # PixelOS
     sweet-playground)
@@ -91,7 +50,7 @@ case "$DEVICE_IMPORT" in
         export ACTUAL_MAIN_DEFCONFIG="sweet_defconfig"
         export COMMON_DEFCONFIG="vendor/debugfs.config"
         export DEVICE_DEFCONFIG=""
-        export KERNEL_NAME="-VantomKernel-neon"
+        export KERNEL_NAME="-vantom-neon"
         ;;
     # OneUI
     a9y18qlte)
@@ -103,44 +62,28 @@ case "$DEVICE_IMPORT" in
         export KERNEL_VERSION="4.4"
         ;;
     *)
-        echo "- Invalid DEVICE_IMPORT. Valid options: sweet, davinci, ginkgo, laurel_sprout, mi89x7, mi89x7-playground, mi89x7-droidspaces, a52s, a9y18qlte, sweet-playground, sweet-droidspaces. Yours: $DEVICE_IMPORT."
+        echo "- Invalid DEVICE_IMPORT. Valid options: sweet, davinci, ginkgo, laurel_sprout, mi89x7-playground, a9y18qlte. Yours: $DEVICE_IMPORT."
         exit 1
         ;;
 esac
 
-# Maintainer info
-export GIT_NAME="$KBUILD_BUILD_USER"
-export GIT_EMAIL="$KBUILD_BUILD_USER@$KBUILD_BUILD_HOST"
-
-# Clang and GCC late Settings
-if [[ "$TC_ALT_MODE" == "0" ]]; then
-    export TC_URLS_REAL=("${TC_URLS_MAIN[@]}")
-    # Global Make Arguments
-    export MAKE_ARGS=(
-        ARCH=arm64 LLVM=1 LLVM_IAS=1 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as
-        NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
-        CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
-        KCFLAGS="-Wno-implicit-enum-enum-cast -Wno-default-const-init-var-unsafe -Wno-error"
-    )
-elif [[ "$TC_ALT_MODE" == "1" ]]; then
-    export TC_URLS_REAL=("${TC_URLS_LEGACY[@]}")
-    # Global Make Arguments
-    export MAKE_ARGS=(
+# GCC and Clang settings
+echo "-- Exporting toolchain settings..."
+export CLANG_ROOT="$PWD/clang"
+export GCC64_ROOT="$PWD/gcc64"
+export GCC32_ROOT="$PWD/gcc32"
+export PATH="$CLANG_ROOT/bin:$GCC64_ROOT/bin:$GCC32_ROOT/bin:/usr/bin:$PATH"
+export MAKE_ARGS=(
         ARCH=arm64 LLVM=1 LLVM_IAS=1 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as
         NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
         CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
         CLANG_TRIPLE=aarch64-linux-gnu-
-    )
-elif [[ "$TC_ALT_MODE" == "2" ]]; then
-    export TC_URLS_REAL=("${TC_URLS_ALT[@]}")
-    # Global Make Arguments
-    export MAKE_ARGS=(
-        ARCH=arm64 LLVM=1 LLVM_IAS=1 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as
-        NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
-        CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
-        CLANG_TRIPLE=aarch64-linux-gnu-
-    )
-fi
+)
+TC_URLS=(
+    "clang|https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b.git"
+    "gcc64|https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git"
+    "gcc32|https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git"
+)
 
 # Clang and GCC Setup
 for tc in "${TC_URLS_REAL[@]}"; do
@@ -150,40 +93,6 @@ for tc in "${TC_URLS_REAL[@]}"; do
             echo "-- Cloning $dir..."
             rm -rf "$dir"
             git clone "$url" --depth=1 "$dir" &> /dev/null || { echo "-- Fatal: Failed to clone $dir!"; exit 1; }
-        else
-            echo "-- Using local $dir"
-        fi
-    else
-        if [ ! -d "$dir" ]; then
-            echo "-- Downloading $dir..."
-            if [[ "$dir" == "gcc64" ]]; then
-                search="eva-gcc-arm64"
-                compress="xz"
-            elif [[ "$dir" == "gcc32" ]]; then
-                search="eva-gcc-arm-"
-                compress="xz"
-            else
-                search="clang-r[0-9]+[a-z]?"
-                compress="gz"
-            fi
-            asset_url=$(curl -sL -H "User-Agent: bash-script" "$url" \
-                | grep -oP "https://github\.com/[^\"]+${search}[^\"]+(\.tar\.gz|\.xz)" \
-                | head -n 1)
-            if [ -z "$asset_url" ]; then
-                echo "-- Fatal: Could not find a valid download link for $dir!"
-                exit 1
-            fi
-            echo "-- URL: $asset_url"
-            mkdir -p "$dir"
-            if [[ "$compress" == "gz" ]]; then
-                curl -sL "$asset_url" -o "$dir.tar.gz" || { echo "-- Fatal: Failed to download $dir!"; exit 1; }
-                tar -xzf "$dir.tar.gz" -C "$dir" || { echo "-- Fatal: Failed to extract $dir!"; exit 1; }
-                rm -f "$dir.tar.gz"
-            else
-                curl -sL "$asset_url" -o "$dir.tar" || { echo "-- Fatal: Failed to download $dir!"; exit 1; }
-                tar -xf "$dir.tar" -C "$dir" || { echo "-- Fatal: Failed to extract $dir!"; exit 1; }
-                rm -f "$dir.tar"
-            fi
         else
             echo "-- Using local $dir"
         fi
@@ -211,6 +120,7 @@ if [ "$DEVICE_IMPORT" == "a9y18qlte" ]; then
     export LD_LIBRARY_PATH="$OPENSSL_DIR/lib:$LD_LIBRARY_PATH"
     export MY_OPENSSL_DIR="$OPENSSL_DIR"
 
+    echo "-- Setting up MAKE_ARGS..."
     export MAKE_ARGS=(
         ARCH=arm64 CC=aarch64-linux-android-gcc LD=aarch64-linux-android-ld.bfd
         AR=aarch64-linux-android-ar AS=aarch64-linux-android-as NM=aarch64-linux-android-nm
