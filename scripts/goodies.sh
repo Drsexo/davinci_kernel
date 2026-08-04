7e6394c3da59a9acd08ac222c622db453d575868
riarumoda <justinebudiwijaya@gmail.com>
2026-08-04 18:55:46 +0800

    goodies: temporarily disable assets goodies reverts
    Signed-off-by: riarumoda <justinebudiwijaya@gmail.com>

--- scripts/goodies.sh
+++ scripts/goodies.sh
@@ -5,9 +5,9 @@ echo "- Setting up additional goodies..."
 git clone https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd.git scripts/goodies/assets &> /dev/null
 # Reset goodies repository to a specific commit
 # Latest commit have iissues so we need to revert it back the janky way.
-cd scripts/goodies/assets
-git reset --hard 75f1699a7f0a270ecae7ba696315e3d55d643101 &> /dev/null
-cd ../../../
+# cd scripts/goodies/assets
+# git reset --hard 75f1699a7f0a270ecae7ba696315e3d55d643101 &> /dev/null
+# cd ../../../
 
 # KernelSU
 chmod +x scripts/goodies/kernelsu.sh
