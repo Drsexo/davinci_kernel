e5eed3a3f2aab8f1d70d40f4a3129bc5285b7ce3
riarumoda <justinebudiwijaya@gmail.com>
2026-08-10 09:31:00 +0800

    patches: discard rmnet as builtin for every builds
    it causes ton of issues on mi89x7 when sim card are inserted.
    
    Signed-off-by: riarumoda <justinebudiwijaya@gmail.com>

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -89,12 +89,6 @@ case "$DEVICE_IMPORT" in
             echo "-- Allowing to compile on new AOSP clang..."
             sed -i 's/-Wno-format-security/-Wno-format-security -Wno-enum-conversion -Wno-default-const-init-var-unsafe -Wno-default-const-init-field-unsafe -Wno-implicit-enum-enum-cast/g' Makefile
         fi
-        # Set drivers as built-in for 4.14
-        if [[ "$DEVICE_IMPORT" == "ginkgo" ]] || [[ "$DEVICE_IMPORT" == "laurel_sprout" ]]; then
-            echo "-- Setting up drivers as built-in..."
-            sed -i 's/default m/default y/g' techpack/data/drivers/rmnet/perf/Kconfig
-            sed -i 's/default m/default y/g' techpack/data/drivers/rmnet/shs/Kconfig
-        fi
         # Common configs for 4.14
         echo "-- Tuning default configs..."
         if [[ "$DEVICE_IMPORT" != "sweet-playground" ]]; then
@@ -148,22 +142,17 @@ case "$DEVICE_IMPORT" in
             }' techpack/xiaomi-msm8937/touchscreen/focaltech_touch/focaltech_point_report_check.c
         fi
         # Set drivers as built-in for 4.19
-        if [[ "$DEVICE_IMPORT" == "gta4l" || "$DEVICE_IMPORT" == "umi" || "$DEVICE_IMPORT" == "cmi" ]]; then
-            echo "-- Setting up drivers as built-in..."
-            sed -i 's/default m/default y/g' techpack/data/drivers/rmnet/perf/Kconfig
-            sed -i 's/default m/default y/g' techpack/data/drivers/rmnet/shs/Kconfig
+        if [[ "$DEVICE_IMPORT" == "gta4l" ]]; then
+            echo "-- Fixing scripts/dtc/livetree.c..."
+            sed -i '/assert(generate_fixups);/d' scripts/dtc/livetree.c
             if [[ "$DEVICE_IMPORT" == "gta4l" ]]; then
-                echo "-- Fixing scripts/dtc/livetree.c..."
-                sed -i '/assert(generate_fixups);/d' scripts/dtc/livetree.c
-                if [[ "$DEVICE_IMPORT" == "gta4l" ]]; then
-                    echo "-- Setting up extra drivers as built-in for gta4l..."
-                    sed -i 's/^CONFIG_QCA_CLD_WLAN=m$/CONFIG_QCA_CLD_WLAN=y/' arch/arm64/configs/$DEVICE_DEFCONFIG
-                    find techpack/data -name "Makefile" -exec sed -i 's/obj-m/obj-y/g' {} +
-                    find techpack/audio/config -name "*.conf" -exec sed -i 's/=m/=y/g' {} +
-                    find techpack/audio -name "Makefile*" -exec sed -i 's/obj-m/obj-y/g' {} +
-                    find techpack/audio -name "Kbuild*" -exec sed -i 's/obj-m/obj-y/g' {} +
-                    echo "CONFIG_SENSORS_SSC=y" >> $MAIN_DEFCONFIG
-                fi
+                echo "-- Setting up extra drivers as built-in for gta4l..."
+                sed -i 's/^CONFIG_QCA_CLD_WLAN=m$/CONFIG_QCA_CLD_WLAN=y/' arch/arm64/configs/$DEVICE_DEFCONFIG
+                find techpack/data -name "Makefile" -exec sed -i 's/obj-m/obj-y/g' {} +
+                find techpack/audio/config -name "*.conf" -exec sed -i 's/=m/=y/g' {} +
+                find techpack/audio -name "Makefile*" -exec sed -i 's/obj-m/obj-y/g' {} +
+                find techpack/audio -name "Kbuild*" -exec sed -i 's/obj-m/obj-y/g' {} +
+                echo "CONFIG_SENSORS_SSC=y" >> $MAIN_DEFCONFIG
             fi
         fi
         # Set cpu tuning for 4.19
