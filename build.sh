a0fe72a2fd767b2affdbdbe2d43d63044ac45233
riarumoda <justinebudiwijaya@gmail.com>
2026-08-14 10:30:27 +0800

    main: bump to version 2.0
    Signed-off-by: riarumoda <justinebudiwijaya@gmail.com>

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ echo " | |_) / _ \ '__| |_  |  \| |/ _ \/ _ \| '_ \  "
 echo " |  __/  __/ |  |  _| | |\  |  __/ (_) | | | | "
 echo " |_|   \___|_|  |_|   |_| \_|\___|\___/|_| |_| "
 echo "==============================================="
-echo " Build Script 1.9 - by Riaru Moda"
+echo " Build Script 2.0 - by Riaru Moda"
 echo " https://t.me/trrflex"
 echo " "
 
