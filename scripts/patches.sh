8de2b8d5a1231af3bf064fa1da9a50881362ad7e
riarumoda <justinebudiwijaya@gmail.com>
2026-08-16 17:13:58 +0800

    devices, patches: completely discard lto on zelo
    also set back clang_strat to 0
    
    Signed-off-by: riarumoda <justinebudiwijaya@gmail.com>

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -311,8 +311,6 @@ case "$DEVICE_IMPORT" in
         echo "CONFIG_SECURITY_SELINUX_DEVELOP=y" >> $MAIN_DEFCONFIG
         echo "CONFIG_KALLSYMS_ALL=y" >> $MAIN_DEFCONFIG
         echo "CONFIG_CHECKPOINT_RESTORE=y" >> $MAIN_DEFCONFIG
-        echo "CONFIG_LTO_CLANG=y" >> $MAIN_DEFCONFIG
-        echo "CONFIG_THINLTO=y" >> $MAIN_DEFCONFIG
     ;;
     *)
         echo "No specific patches to apply for $DEVICE_IMPORT."
